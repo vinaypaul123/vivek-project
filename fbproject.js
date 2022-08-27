0eaa5ce53d6e83801e86eacc3e1722e5abeb7898
vinay <vinaypaulkarimathy@gmail.com>
2022-08-27 16:23:19 +0530

    Vivek file

---
+++ fbproject.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:6
+            },
+            Day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            male:{
+                required:true
+            },
+        },
+        messages:{
+            fname:{
+                required:"Enter first name",
+                minlength:"Enter at least 4 characters"
+            }
+        }
+    })
+})
